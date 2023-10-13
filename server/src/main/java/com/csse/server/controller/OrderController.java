3825a48786e123789fcbc1ddba686db18540282e
ch4mi2 <chamithu4@gmail.com>
2023-10-13 21:25:03 +0530

    state fix

--- server/src/main/java/com/csse/server/controller/OrderController.java
+++ server/src/main/java/com/csse/server/controller/OrderController.java
@@ -7,13 +7,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/orders/")
@@ -27,13 +21,13 @@ public class OrderController {
         System.out.println("Get all Orders");
         return new ResponseEntity<List<Order>>(orderService.allOrders(), HttpStatus.OK);
     }
-    //Changed this . why? idk
-    //@PostMapping("/changeState")
-    @PostMapping("/{id}{changeState}")
+
+    @PatchMapping("/state/{id}")
     public ResponseEntity<String> changeOrderState(
-            @RequestParam ObjectId orderId,  //Changed this . why? idk
-            @RequestParam String newState) {
-        String result = orderService.changeOrderState(orderId, newState);
+            @PathVariable ObjectId id,
+            @RequestBody String newState) {
+        System.out.println(newState);
+        String result = orderService.changeOrderState(id, newState);
         if (result != null) {
             return new ResponseEntity<>(result, HttpStatus.CREATED);
         } else {
