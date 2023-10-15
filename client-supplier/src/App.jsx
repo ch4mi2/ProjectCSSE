fb77039a1b9087c946ba6a75dbf28d54190fd212
ch4mi2 <chamithu4@gmail.com>
2023-10-15 21:21:14 +0530

    feat : view orders

--- client-supplier/src/App.jsx
+++ client-supplier/src/App.jsx
@@ -1,8 +1,26 @@
 import Routes from "./routes/routes";
 import SupplierContext from "./context/SupplierContext";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 function App() {
   const [supplier, setSupplier] = useState(null);
+
+  // Load the supplier from local storage on component mount
+  useEffect(() => {
+    console.log("use effect fetch");
+    const storedSupplier = localStorage.getItem("supplier");
+    console.log(storedSupplier);
+    if (storedSupplier) {
+      setSupplier(JSON.parse(storedSupplier));
+    }
+  }, []);
+  
+  // Save the supplier to local storage whenever it changes
+  useEffect(() => {
+    console.log("use effect save");
+    localStorage.setItem("supplier", JSON.stringify(supplier));
+  }, [supplier]);
+  
 
   return (
     <SupplierContext.Provider value={{ supplier, setSupplier }}>
