d5a4c3130e9b7f1832cd5ba6fe86bcd74c3e1ed1
ch4mi2 <chamithu4@gmail.com>
2023-10-16 18:42:49 +0530

    invoice

--- client-supplier/src/pages/CreateInvoice.jsx
+++ client-supplier/src/pages/CreateInvoice.jsx
@@ -1,85 +1,164 @@
-// import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-// const CreateInvoice = ({ order }) => {
-//   const [invoiceData, setInvoiceData] = useState({
-//     invoiceNumber: '',  // You can generate this dynamically
-//     issueDate: new Date().toLocaleDateString(),
-//     dueDate: '',
-//     total: order.total,
-//     site: order.site,
-//     sitemanager: order.sitemanager,
-//     items: order.items,
-//     supplierPrice: order.supplier.price,
-//     billingAddress: {
-//       name: '',
-//       street: '',
-//       city: '',
-//       zip: '',
-//     },
-//     shippingAddress: {
-//       name: '',
-//       street: '',
-//       city: '',
-//       zip: '',
-//     },
-//     paymentTerms: '',
-//     notes: '',
-//   });
+const CreateInvoice = () => {
+  const { orderId } = useParams();
+  const [order, setOrder] = useState({});
+  const [invoiceData, setInvoiceData] = useState({
+    siteName: '',
+    siteAddress: '',
+    invoiceDate: '',
+    items: {},
+    total: 0, // Initialize the total with 0
+  });
 
-//   const handleCreateInvoice = () => {
-//     // Handle the creation of the invoice data, e.g., sending to a server
-//     console.log('Invoice Data:', invoiceData);
-//   };
+  useEffect(() => {
+    const fetchOrder = async () => {
+      const response = await fetch(`/api/orders/${orderId}`);
+      const data = await response.json();
 
-//   return (
-//     <div>
-//       <h1>Create Invoice</h1>
-//       <form>
-//         <div>
-//           <label htmlFor="invoiceNumber">Invoice Number:</label>
-//           <input
-//             type="text"
-//             id="invoiceNumber"
-//             name="invoiceNumber"
-//             value={invoiceData.invoiceNumber}
-//             onChange={(e) => setInvoiceData({ ...invoiceData, invoiceNumber: e.target.value })}
-//           />
-//         </div>
-//         <div>
-//           <label htmlFor="issueDate">Issue Date:</label>
-//           <input
-//             type="text"
-//             id="issueDate"
-//             name="issueDate"
-//             value={invoiceData.issueDate}
-//             readOnly
-//           />
-//         </div>
-//         <div>
-//           <label htmlFor="dueDate">Due Date:</label>
-//           <input
-//             type="text"
-//             id="dueDate"
-//             name="dueDate"
-//             value={invoiceData.dueDate}
-//             onChange={(e) => setInvoiceData({ ...invoiceData, dueDate: e.target.value })}
-//           />
-//         </div>
-//         <div>
-//           <label htmlFor="total">Total:</label>
-//           <input
-//             type="text"
-//             id="total"
-//             name="total"
-//             value={invoiceData.total}
-//             readOnly
-//           />
-//         </div>
-//         {/* Include more invoice details as needed */}
-//         <button onClick={handleCreateInvoice}>Create Invoice</button>
-//       </form>
-//     </div>
-//   );
-// };
+      if (response.ok) {
+        setOrder(data);
+        setInvoiceData({
+          siteName: data.mainSite.name || '',
+          siteAddress: data.mainSite.address || '',
+          invoiceDate: '', // Initialize with an empty date
+          items: data.items || {},
+          total: data.total || 0, // Get the total from the order data
+        });
+      }
+    };
 
-// export default CreateInvoice;
+    fetchOrder();
+  }, [orderId]);
+
+  const handleCreateInvoice = () => {
+    // Prepare the data to send
+    const dataToSend = {
+      siteName: invoiceData.siteName,
+      siteAddress: invoiceData.siteAddress,
+      invoiceDate: invoiceData.invoiceDate,
+      items: invoiceData.items,
+      total: invoiceData.total,
+    };
+  
+    // Send the data to the backend
+    fetch('/api/invoice/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dataToSend),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log('Invoice data sent successfully.');
+          alert('Invoice created successfully.');
+          // Handle any additional actions after successful submission
+        } else {
+          console.error('Failed to send invoice data.');
+          alert('Failed to create invoice.');
+          // Handle errors, e.g., display an error message
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        // Handle network errors, e.g., display an error message
+      });
+  };
+  
+
+  const updateItemQuantity = (itemName, quantity) => {
+    const updatedItems = { ...invoiceData.items };
+    updatedItems[itemName] = quantity;
+    setInvoiceData({ ...invoiceData, items: updatedItems });
+    calculateTotal(updatedItems); // Calculate the total when item quantities change
+  };
+
+  const calculateTotal = (items) => {
+    const total = Object.keys(items).reduce((acc, itemName) => {
+      return acc + items[itemName];
+    }, 0);
+    setInvoiceData({ ...invoiceData, total });
+  };
+
+  return (
+    <div className="max-w-3xl mx-auto p-4">
+      <h1 className="text-2xl font-semibold mb-4">Create Invoice</h1>
+      <div className="bg-white p-4 shadow-lg rounded-lg">
+        <div className="mb-4">
+          <h2 className="text-lg font-semibold mb-2">Site Information</h2>
+          <div className="mb-2">
+            <label className="block text-gray-600">Site Name:</label>
+            <input
+              type="text"
+              className="w-full border rounded px-2 py-1"
+              value={invoiceData.siteName}
+              onChange={(e) =>
+                setInvoiceData({ ...invoiceData, siteName: e.target.value })
+              }
+            />
+          </div>
+          <div className="mb-2">
+            <label className="block text-gray-600">Site Address:</label>
+            <input
+              type="text"
+              className="w-full border rounded px-2 py-1"
+              value={invoiceData.siteAddress}
+              onChange={(e) =>
+                setInvoiceData({ ...invoiceData, siteAddress: e.target.value })
+              }
+            />
+          </div>
+          <div className="mb-2">
+            <label className="block text-gray-600">Invoice Date:</label>
+            <input
+              type="date"
+              className="w-full border rounded px-2 py-1"
+              value={invoiceData.invoiceDate}
+              onChange={(e) =>
+                setInvoiceData({ ...invoiceData, invoiceDate: e.target.value })
+              }
+            />
+          </div>
+        </div>
+        <div className="mb-4">
+          <h2 className="text-lg font-semibold mb-2">Items</h2>
+          <ul>
+            {Object.keys(invoiceData.items).map((itemName, index) => (
+              <li
+                key={index}
+                className="flex justify-between items-center border-b py-2"
+              >
+                <span>{itemName}:</span>
+                <input
+                  type="number"
+                  value={invoiceData.items[itemName]}
+                  onChange={(e) =>
+                    updateItemQuantity(
+                      itemName,
+                      parseInt(e.target.value, 10)
+                    )
+                  }
+                  className="border rounded px-2 py-1 w-16 text-center"
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="mb-4">
+          <h2 className="text-lg font-semibold mb-2">Total</h2>
+          <p className="font-semibold text-xl">${invoiceData.total}</p>
+        </div>
+        <button
+          onClick={handleCreateInvoice}
+          className="bg-yellow-300 text-black py-2 px-4 rounded hover:bg-yellow-400"
+        >
+          Send Invoice
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CreateInvoice;
--- server/src/main/java/com/csse/server/repository/InvoiceRepository.java
+++ server/src/main/java/com/csse/server/repository/InvoiceRepository.java
@@ -1,2 +1,10 @@
-package com.csse.server.repository;public class InvoiceRepository {
+package com.csse.server.repository;
+
+
+import com.csse.server.model.Invoice;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface InvoiceRepository extends MongoRepository<Invoice, ObjectId> {
+
 }
