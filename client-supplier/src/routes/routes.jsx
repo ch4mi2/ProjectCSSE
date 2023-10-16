208313c2bea0bb9a4f05d61f35ca78627a4139d5
ch4mi2 <chamithu4@gmail.com>
2023-10-16 12:38:43 +0530

    feat: supplier done

--- client-supplier/src/routes/routes.jsx
+++ client-supplier/src/routes/routes.jsx
@@ -1,22 +1,22 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { useContext } from "react";
-// import Login from "../pages/Login";
-// import Navbar from "../components/Navbar";
-// import SupplierContext from "../context/SupplierContext";
-// import Dashboard from "../pages/Dashboard";
-// import CreateInvoice from "../pages/CreateInvoice";
-// const AppRoutes = () => {
-//     const { supplier } = useContext(SupplierContext);
-//   return (
-//     <Router>
-//       {supplier && <Navbar />}
-//       <Routes>
-//         <Route exact path="/" element={<Login />} />
-//         <Route path="/dashboard" element={<Dashboard />} />
-//         <Route path="/create-invoice/:orderId" element={<CreateInvoice />} />
-//       </Routes>
-//     </Router>
-//   );
-// };
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import Login from "../pages/Login";
+import Navbar from "../components/Navbar";
+import SupplierContext from "../context/SupplierContext";
+import Dashboard from "../pages/Dashboard";
+//import CreateInvoice from "../pages/CreateInvoice";
+const AppRoutes = () => {
+    const { supplier } = useContext(SupplierContext);
+  return (
+    <Router>
+      {supplier && <Navbar />}
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/create-invoice/:orderId" element={<CreateInvoice />} /> */}
+      </Routes>
+    </Router>
+  );
+};
 
-// export default AppRoutes;
+export default AppRoutes;
